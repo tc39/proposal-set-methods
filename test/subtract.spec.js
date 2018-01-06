7181eda115ac14d38ccef6449349e0792c2ec2d8
Sathya Gunasekaran <gsathya@chromium.org>
2018-01-05 16:19:43 -0800

    Update spec/polyfill
    Spec changes
    
    - Fixed #6 (create a new set with items populated in both union and subtract)
    - Changed .subtract to return newSet after iteration is done
    - Use 'items' instead of 'iterables' to be more consistent
    
    Polyfill changes
    
    - Added SpeciesConstructor, isObject, isCallable
    - Removed un spec'd xor
    - Updated subtract, union, removeElements
    
    Added tests

--- test/subtract.spec.js
+++ test/subtract.spec.js
@@ -2,10 +2,52 @@
 
 const {assert} = require('chai');
 
-describe.skip('Set.prototype.subtract', () => {
+describe('Set.prototype.subtract', () => {
     it('Should be present', () => {
         const set = new Set();
         assert.isFunction(set.subtract);
         assert.isFunction(Set.prototype.subtract);
-    })
+    });
+
+    it('Should subtract elements from single Set', () => {
+        const set = new Set([1, 2]);
+        const otherSet = new Set([1, 3]);
+        const actual = set.subtract(otherSet);
+        const expected = new Set([2]);
+        assert.deepEqual(...actual, ...expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
+    });
+
+    it('Should subtract elements from single Array', () => {
+        const set = new Set([1, 2]);
+        const otherArray = [1, 3];
+        const actual = set.subtract(otherArray);
+        const expected = new Set([2]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
+    });
+
+    it('Should subtract elements from multiple Sets', () => {
+        const set = new Set([1, 2, 3]);
+        const set2 = new Set([1, 4]);
+        const set3 = new Set([2, 5]);
+        const actual = set.subtract(set2, set3);
+        const expected = new Set([3]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
+    });
+
+    it('Should subtract elements from multiple iterables', () => {
+        const set = new Set([1, 2, 3]);
+        const set2 = new Set([1, 4]);
+        const set3 = [2, 5];
+        const actual = set.subtract(set2, set3);
+        const expected = new Set([3]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
+    });
 })
--- test/union.spec.js
+++ test/union.spec.js
@@ -14,52 +14,46 @@ describe('Set.prototype.union', () => {
         assert.throws(()=>union([]));
     });
 
-    it('Should throw when called on invalid receiver', () => {
-        const union = Set.prototype.union;
-        assert.throws(()=>Reflect.apply(union, {}, [new Set()]));
-    });
-
-
     it('Should add elements from single Set', () => {
         const set = new Set([1, 2]);
-        const otherSet = new Set([4, 5]);
-        const resultSet = set.union(otherSet);
-        const elements = [...resultSet];
-        assert.deepEqual(elements, [1, 2, 4, 5]);
-        assert.instanceOf(resultSet, Set);
-        assert.strictEqual(resultSet.constructor, Set);
+        const otherSet = new Set([1, 4, 5]);
+        const actual = set.union(otherSet);
+        const expected = new Set([1, 2, 4, 5]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
     });
 
     it('Should add elements from single Array', () => {
         const set = new Set([1, 2]);
-        const otherArray = [4, 5];
-        const resultSet = set.union(otherArray);
-        const elements = [...resultSet];
-        assert.deepEqual(elements, [1, 2, 4, 5]);
-        assert.instanceOf(resultSet, Set);
-        assert.strictEqual(resultSet.constructor, Set);
+        const otherArray = [1, 4, 5];
+        const actual = set.union(otherArray);
+        const expected = new Set([1, 2, 4, 5]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
     });
 
     it('Should add elements from multiple Sets', () => {
         const set = new Set([1, 2]);
-        const set2 = new Set([4, 5]);
-        const set3 = new Set([6, 7]);
-        const resultSet = set.union(set2, set3);
-        const elements = [...resultSet];
-        assert.deepEqual(elements, [1, 2, 4, 5, 6, 7]);
-        assert.instanceOf(resultSet, Set);
-        assert.strictEqual(resultSet.constructor, Set);
+        const set2 = new Set([1, 4, 5]);
+        const set3 = new Set([4, 6, 7]);
+        const actual = set.union(set2, set3);
+        const expected = new Set([1, 2, 4, 5, 6, 7]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
     });
 
-    it('Should add elements from multiple Sets', () => {
+    it('Should add elements from multiple iterables', () => {
         const set = new Set([1, 2]);
-        const set2 = new Set([4, 5]);
-        const set3 = [6, 7];
-        const resultSet = set.union(set2, set3);
-        const elements = [...resultSet];
-        assert.deepEqual(elements, [1, 2, 4, 5, 6, 7]);
-        assert.instanceOf(resultSet, Set);
-        assert.strictEqual(resultSet.constructor, Set);
+        const set2 = new Set([1, 4, 5]);
+        const set3 = [4, 6, 7];
+        const actual = set.union(set2, set3);
+        const expected = new Set([1, 2, 4, 5, 6, 7]);
+        assert.deepEqual(actual, expected);
+        assert.instanceOf(actual, Set);
+        assert.strictEqual(actual.constructor, Set);
     });
 
     describe.skip('Subclassing without overwriting @@species', () => {
